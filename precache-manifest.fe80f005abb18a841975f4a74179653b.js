efe203750afe7a889b5d0ff1db08d50583a2f367
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-10-13 00:54:16 +0300

    Updates

---
+++ precache-manifest.fe80f005abb18a841975f4a74179653b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc83f3feacdc274ef9346170fc850f0",
+    "url": "/react-pie-chart/index.html"
+  },
+  {
+    "revision": "07e5702855e44ea87560",
+    "url": "/react-pie-chart/static/js/2.b300dddd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pie-chart/static/js/2.b300dddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db80709853dbae3313e",
+    "url": "/react-pie-chart/static/js/main.5c3477f4.chunk.js"
+  },
+  {
+    "revision": "a63287404b031644fe89",
+    "url": "/react-pie-chart/static/js/runtime-main.583b07dd.js"
+  }
+]);
--- src/utils/getRandomColor.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  let c = {
-    r: Math.floor(0),
-    g: Math.floor(130+Math.random()*100),
-    b: Math.floor(155+Math.random()*100)
-  };
-  c.rgb = 'rgb('+c.r+','+c.g+','+c.b+')';
-  return c.rgb;
-};
